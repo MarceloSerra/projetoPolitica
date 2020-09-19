3ea74f29235e09d981c189f3fc2b28236172ea99
Gabi Pires <gabrielatrindadepires@gmail.com>
2020-09-19 12:04:22 -0300

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EducacaoComponent } from './components/educacao/educacao.component';
+import { HabitacaoComponent } from './components/habitacao/habitacao.component';
 import { MenuPagLayoutComponent } from './components/menu-pag-layout/menu-pag-layout.component';
+import { MobilidadeAcessibilidadeComponent } from './components/mobilidade-acessibilidade/mobilidade-acessibilidade.component';
+import { PlanUrbanoComponent } from './components/plan-urbano/plan-urbano.component';
+import { SaudeSaneamentoComponent } from './components/saude-saneamento/saude-saneamento.component';
 import { SobrePageComponent } from './components/sobre-page/sobre-page.component';
 
 
 const routes: Routes = [
   {
     path: '', component: MenuPagLayoutComponent,
-    children: [
-      { path: '', component: ExemploComponent },
-      { path: 'sobre', component: SobrePageComponent },
-    ]
   },
+  {
+    path: 'educacao', component: EducacaoComponent,
+  },
+  {
+    path: 'habitacao', component: HabitacaoComponent,
+  },
+  {
+    path: 'mobacsss', component: MobilidadeAcessibilidadeComponent,
+  },
+  {
+    path: 'planurbano', component: PlanUrbanoComponent,
+  },
+  {
+    path: 'saudesanem', component: SaudeSaneamentoComponent,
+  },
+
 ];
 
 @NgModule({
--- src/app/components/mobilidade-acessibilidade/mobilidade-acessibilidade.component.ts
+++ src/app/components/mobilidade-acessibilidade/mobilidade-acessibilidade.component.ts
@@ -10,6 +10,7 @@ export class MobilidadeAcessibilidadeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
